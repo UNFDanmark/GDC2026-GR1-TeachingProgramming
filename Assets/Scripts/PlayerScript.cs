7d6bc6f630412cc8ab3b0c0cc284599331becf20
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-05 12:29:41 +0200

    Animation
    Still need shoot for skellies

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -12,19 +12,33 @@ public class PlayerScript : MonoBehaviour
     public float cameraSensY;
     public GameObject cameraGameObject;
     Rigidbody rb;
+    Animator animator;
     public float moveSpeed = 100f;
+    public GameObject bulletPrefab;
+    public GameObject shootingPoint;
+    public float bulletspeed = 5;
+    GameObject bullet;
+    public InputActionReference actionShoot;
+    public float bulletExpire = 2.0f;
+    public float Cooldown = 10.0f;
+    float CooldownLeft;
     void Start()
     {
         actionMovement.action.Enable();
         actionLook.action.Enable();
+        actionShoot.action.Enable();
+        actionShoot.action.started += Shoot;
         print("Ich bin Søren");
         rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void OnDisable()
     {
         actionMovement.action.Disable();
         actionLook.action.Disable();
+        actionShoot.action.Disable();
+        actionShoot.action.started -= Shoot;
     }
 
     void Update()
@@ -38,6 +52,9 @@ public class PlayerScript : MonoBehaviour
         Vector3 move = new Vector3(rawMoveInput.x, 0, rawMoveInput.y);
         move *= moveSpeed * rb.linearDamping * Time.deltaTime;
         rb.AddRelativeForce(move);
+
+        animator.SetFloat("speed", rb.linearVelocity.magnitude);
+        CooldownLeft -= Time.deltaTime;
     }
     void OnTriggerEnter(Collider collision)
     {
@@ -49,6 +66,18 @@ public class PlayerScript : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy"))
         {
             print("Du tabte");
+        }
+    }
+
+    void Shoot(InputAction.CallbackContext context)
+    {
+        if (CooldownLeft <= 0)
+        {
+            bullet = Instantiate(bulletPrefab, shootingPoint.transform.position, shootingPoint.transform.rotation);
+            bullet.GetComponentInChildren<Rigidbody>().AddForce(transform.forward * bulletspeed, ForceMode.Impulse);
+            GameObject.Destroy(bullet, bulletExpire);
+            animator.SetTrigger("shoot");
+            CooldownLeft = Cooldown;
         }
     }
 }
